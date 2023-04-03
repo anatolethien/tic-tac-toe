22c9ad229f374e8f961ff0d54791251711e280ee
anatolethien <mail@anatolethien.eu>
2023-04-03 12:19:27 +0200

    __str__ and winning position check

--- app.py
+++ app.py
@@ -5,16 +5,39 @@ class TicTacToe:
             [' ', ' ', ' '],
             [' ', ' ', ' '],
         ]
-        self.player1 = input('Player 1: ')
-        self.player2 = input('Player 2: ')
-
-    def display_grid(self):
-        print('\n-+-+-\n'.join(['|'.join(row) for row in self.grid]))
+        # self.player1 = input('Player 1: ')
+        # self.player2 = input('Player 2: ')
+    def __str__(self):
+        return '\n-+-+-\n'.join(['|'.join(row) for row in self.grid])
 
     def encode_grid(self) -> str:
+        """Returns a string representation of the grid."""
         return ''.join([''.join(row) for row in self.grid])
+    
+    def is_winning_position(self) -> bool:
+        grid = self.encode_grid()
+        return any([
+            grid[0] == grid[1] == grid[2] != ' ',
+            grid[3] == grid[4] == grid[5] != ' ',
+            grid[6] == grid[7] == grid[8] != ' ',
+            grid[0] == grid[3] == grid[6] != ' ',
+            grid[1] == grid[4] == grid[7] != ' ',
+            grid[2] == grid[5] == grid[8] != ' ',
+            grid[0] == grid[4] == grid[8] != ' ',
+            grid[2] == grid[4] == grid[6] != ' '
+        ])
+    
+    def play(self, player: str, x: int, y: int):
+        if self.grid[x-1][y-1] == ' ':
+            self.grid[x-1][y-1] = player
 
 if __name__ == '__main__':
     ttc = TicTacToe()
-    ttc.display_grid()
-    print(ttc.encode_grid())
+    ttc.play('O', 1, 1)
+    ttc.play('X', 1, 2)
+    ttc.play('O', 2, 2)
+    ttc.play('X', 2, 1)
+    ttc.play('O', 3, 3)
+    # ttc.display_grid()
+    print(ttc)
+    print(ttc.is_winning_position())
