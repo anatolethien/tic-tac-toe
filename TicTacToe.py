8f4a0058bf355388706100d0aee537c91992c9bd
anatolethien <mail@anatolethien.eu>
2023-04-04 11:20:32 +0200

    encapsulation and docstring

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,38 +1,45 @@
 class TicTacToe:
+    """
+    TicTacToe is a two-player game played on a three-by-three grid. The two
+    players take turns marking either `X` or `O` on the grid. The first player
+    that places three marks in a row wins.
+    """
     def __init__(self):
-        self.grid = [
+        self.__NOUGHT = 'O'
+        self.__CROSS = 'X'
+        self.__grid = [
             [' ', ' ', ' '],
             [' ', ' ', ' '],
             [' ', ' ', ' '],
         ]
-        self.round = 0
-        self.player = 'O'
+        self.__round = 0
+        self.__player = self.__NOUGHT
 
     def __str__(self) -> str:
-        return '\n-+-+-\n'.join(['|'.join(row) for row in self.grid])
+        return '\n-+-+-\n'.join(['|'.join(row) for row in self.__grid])
 
-    def encode_grid(self) -> str:
-        return ''.join([''.join(row) for row in self.grid])
+    def __encode_grid(self) -> str:
+        return ''.join([''.join(row) for row in self.__grid])
 
-    def has_winner(self) -> bool:
+    def __has_winner(self) -> bool:
         return any([
-            self.grid[0][0] == self.grid[0][1] == self.grid[0][2] != ' ',
-            self.grid[1][0] == self.grid[1][1] == self.grid[1][2] != ' ',
-            self.grid[2][0] == self.grid[2][1] == self.grid[2][2] != ' ',
-            self.grid[0][0] == self.grid[1][0] == self.grid[2][0] != ' ',
-            self.grid[0][1] == self.grid[1][1] == self.grid[2][1] != ' ',
-            self.grid[0][2] == self.grid[1][2] == self.grid[2][2] != ' ',
-            self.grid[0][0] == self.grid[1][1] == self.grid[2][2] != ' ',
-            self.grid[0][2] == self.grid[1][1] == self.grid[2][0] != ' '
+            self.__grid[0][0] == self.__grid[0][1] == self.__grid[0][2] != ' ',
+            self.__grid[1][0] == self.__grid[1][1] == self.__grid[1][2] != ' ',
+            self.__grid[2][0] == self.__grid[2][1] == self.__grid[2][2] != ' ',
+            self.__grid[0][0] == self.__grid[1][0] == self.__grid[2][0] != ' ',
+            self.__grid[0][1] == self.__grid[1][1] == self.__grid[2][1] != ' ',
+            self.__grid[0][2] == self.__grid[1][2] == self.__grid[2][2] != ' ',
+            self.__grid[0][0] == self.__grid[1][1] == self.__grid[2][2] != ' ',
+            self.__grid[0][2] == self.__grid[1][1] == self.__grid[2][0] != ' '
         ])
 
-    def which_player(self):
-        self.player = 'O' if self.round % 2 == 1 else 'X'
+    def __which_player(self):
+        self.__player = self.__NOUGHT if self.__round % 2 == 1 else self.__CROSS
 
-    def round_info(self) -> str:
-        return f'Round {self.round} - Player {self.player}'
+    def __round_info(self) -> str:
+        return f'Round {self.__round} - Player {self.__player}'
     
-    def user_input(self) -> tuple[int, int]:
+    def __user_input(self) -> tuple[int, int]:
         valid = {1, 2, 3}
         x = 0
         y = 0
@@ -41,16 +48,19 @@ class TicTacToe:
             y = int(input('y: '))
         return x, y
 
-    def play(self):
-        while self.round <= 9 and not self.has_winner():
-            self.round += 1
-            self.player = self.which_player()
-            print(self.round_info())
-            x, y = self.user_input()
-            self.move(x, y)
-            print(self)
-        print(f'Player {self.player} wins!')
+    def __move(self, x: int, y: int):
+        if self.__grid[x - 1][y - 1] == ' ':
+            self.__grid[x - 1][y - 1] = self.__player
 
-    def move(self, x: int, y: int):
-        if self.grid[x - 1][y - 1] == ' ':
-            self.grid[x - 1][y - 1] = self.player
+    def play(self):
+        """
+        Starts the game.
+        """
+        while self.__round <= 9 and not self.__has_winner():
+            self.__round += 1
+            self.__player = self.__which_player()
+            print(self.__round_info())
+            x, y = self.__user_input()
+            self.__move(x, y)
+            print(self)
+        print(f'Player {self.__player} wins!')
