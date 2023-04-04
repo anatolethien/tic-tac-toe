3ff83b508793a3b6b881157ffe26ffa5dfb83a87
anatolethien <mail@anatolethien.eu>
2023-04-04 12:01:59 +0200

    pretty symbols and draw

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,19 +1,19 @@
 class TicTacToe:
     """
     TicTacToe is a two-player game played on a three-by-three grid. The two
-    players take turns marking either `X` or `O` on the grid. The first player
+    players take turns marking either `×` or `○` on the grid. The first player
     that places three marks in a row wins.
     """
     def __init__(self):
-        self.__NOUGHT = 'O'
-        self.__CROSS = 'X'
+        self.__CIRCLE = '○'
+        self.__CROSS = '×'
         self.__grid = [
             [' ', ' ', ' '],
             [' ', ' ', ' '],
             [' ', ' ', ' '],
         ]
         self.__round = 0
-        self.__player = self.__NOUGHT
+        self.__player = self.__CIRCLE
 
     def __str__(self) -> str:
         return '\n-+-+-\n'.join(['|'.join(row) for row in self.__grid])
@@ -34,7 +34,7 @@ class TicTacToe:
         ])
 
     def __which_player(self):
-        self.__player = self.__NOUGHT if self.__round % 2 == 1 else self.__CROSS
+        self.__player = self.__CIRCLE if self.__round % 2 == 1 else self.__CROSS
 
     def __round_info(self) -> str:
         return f'Round {self.__round} - Player {self.__player}'
@@ -43,9 +43,13 @@ class TicTacToe:
         valid = {1, 2, 3}
         x = 0
         y = 0
-        while not x in valid and not y in valid:
-            x = int(input('x: '))
-            y = int(input('y: '))
+        if self.__player == self.__CIRCLE:
+            while not x in valid and not y in valid:
+                x = int(input('x: '))
+                y = int(input('y: '))
+        else:
+            # CPU
+            pass
         return x, y
 
     def __move(self, x: int, y: int):
@@ -56,11 +60,15 @@ class TicTacToe:
         """
         Starts the game.
         """
-        while self.__round <= 9 and not self.__has_winner():
+        while self.__round <= 9 or not self.__has_winner():
             self.__round += 1
             self.__which_player()
             print(self.__round_info())
             x, y = self.__user_input()
             self.__move(x, y)
             print(self)
-        print(f'Player {self.__player} wins!')
+        if self.__has_winner():
+            print(f'Player {self.__player} wins!')
+        else:
+            print('Draw!')
+            
